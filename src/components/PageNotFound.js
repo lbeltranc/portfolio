35f63d569b4e2b70ec0dc67e316f08d7069ed70c
lbeltranc <60752209+lbeltranc@users.noreply.github.com>
2020-06-17 12:22:42 -0700

    Modified 404

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
-	<main>
-		<section>
+	<main className="not-found">
+		<section className="not-found-cont">
 			<div className="page-info">
-				<h2>404...Page Not Found</h2>
+				<h2>404<br /> Oops! Page Not Found</h2>
+				<i className="fas fa-frown-open"></i>
 			</div>
-			<div className="about-text">
-				<p>Go to the <Link to={'/'}>home</Link> page.</p>
+			<div className="back-home">
+				<Link to={'/'} className="btn">back to home</Link>
 			</div>
 		</section>
 	</main>
